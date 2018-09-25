43ed2d26a2f11a4fc651e33a175488410d8db1d4
Kyle Sekellick <sekellickkyle@yahoo.com>
2018-09-25 12:20:18 -0400

    adds the Letter Summary exercise

---
+++ LetterSummary.py
@@ -0,0 +1,14 @@
+# Write a letter_histogram program that asks the user for input, and prints a dictionary containing the tally of how many times each letter in the alphabet was used in the word.
+user = input("Please enter a word: ")
+
+word_dict = {}
+
+for i in user:
+    keys = word_dict.keys()
+    if i in keys:
+        word_dict[i] += 1
+    else:
+        word_dict[i] = 1
+        
+print(word_dict)
+
