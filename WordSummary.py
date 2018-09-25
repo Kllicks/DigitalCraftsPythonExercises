edb66c91f89990f75f346a96679972739e964b36
Kyle Sekellick <sekellickkyle@yahoo.com>
2018-09-25 13:28:54 -0400

    makes the users input in lower case

--- WordSummary.py
+++ WordSummary.py
@@ -1,6 +1,7 @@
 # Write a word_histogram program that asks the user for a sentence as its input, and prints a dictionary containing the tally of how many times each word in the alphabet was used in the text
 
 user = input("Please enter a sentence: ")
+user = user.lower()
 user = user.split(' ')
 word_dict = {}
 
