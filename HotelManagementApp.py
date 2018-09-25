5d9b8d63098798653db42909fd3b2f2da675eecd
Kyle Sekellick <sekellickkyle@yahoo.com>
2018-09-25 15:32:18 -0400

    adds the beginning of the hotel management exercises

---
+++ HotelManagementApp.py
@@ -0,0 +1,44 @@
+# Display a menu asking whether to check in or check out.
+# Prompt the user for a floor number, then a room number.
+# If checking in, ask for the number of occupants and what their names are.
+# If checking out, remove the occupants from that room.
+# Do not allow anyone to check into a room that is already occupied.
+# Do not allow checking out of a room that isn't occupied.
+
+hotel = {
+  '1': {
+    '101': ['George Jefferson', 'Wheezy Jefferson'],
+  },
+  '2': {
+    '237': ['Jack Torrance', 'Wendy Torrance'],
+  },
+  '3': {
+    '333': ['Neo', 'Trinity', 'Morpheus']
+  }
+}
+
+for floor in hotel:
+    for room in hotel[floor]:
+        print("floor:%s room:%s" % (floor,room))
+        for occupants in hotel[floor][room]:
+            print("\t\t" + occupants)
+print()
+
+# check = input("Would you like to check in or out? ")
+# check = check.lower()
+
+# if check == "in":
+#     floor = input("What floor would you like to be on? ")
+#     hotel[floor] = {floor}
+#     room = input("What room would you like? ")
+    
+#     occupants = input("How many occupants are their? ")
+#     hotel[occupants]
+#     counter = 1
+#     while counter < int(occupants) + 1:
+#         name = input("Name of occupant %d: " % (counter))
+#         hotel[occupants] = name
+#         counter += 1
+
+# elif check == "out":
+#     print("So you're checking out")
