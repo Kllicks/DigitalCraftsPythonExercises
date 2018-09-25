20452262f2c4037ed92f45cc5ba20873b1579f8b
Kyle Sekellick <sekellickkyle@yahoo.com>
2018-09-25 12:47:31 -0400

    adds word summary histogram exercise

---
+++ WordSummary.py
@@ -0,0 +1,15 @@
+# Write a word_histogram program that asks the user for a sentence as its input, and prints a dictionary containing the tally of how many times each word in the alphabet was used in the text
+
+user = input("Please enter a sentence: ")
+user = user.split(' ')
+word_dict = {}
+
+for i in user:
+    keys = word_dict.keys()
+    if i in keys:
+        word_dict[i] += 1
+    else:
+        word_dict[i] = 1
+        
+print(word_dict)
+
